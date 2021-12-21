c45029839478318dce3d708d018538ab4b0c1b59
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-21 15:53:55 +0530

    Performed TC1 and TC2

--- src/main/java/com/bridgelabz/Day21/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/Day21/MoodAnalyzer.java
@@ -1,9 +1,20 @@
 package com.bridgelabz.Day21;
 
 public class MoodAnalyzer {
+	// creating here method
+	public String analyzeMood(String message) {
 
+		if (message.contains("HAPPY")) {
+			return "HAPPY";
+		} else
+			return "SAD";
+	}
+
+//Driver class
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		// System.out.println(moodAnalyzer.analyzeMood("sad"));
 
 	}
 
--- src/test/java/MoodAnalyzerProb/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerProb/MoodAnalyzerTest.java
@@ -1,5 +1,32 @@
 package MoodAnalyzerProb;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bridgelabz.Day21.MoodAnalyzer;
+
+import junit.framework.Assert;
+
 public class MoodAnalyzerTest {
+	MoodAnalyzer moodAnalyze;
+
+	@Before
+	public void setUp() {
+		moodAnalyze = new MoodAnalyzer();
+	}
+
+	@Test
+	public void testingSadMood() {
+		String message = "I am in SAD Mood";
+		String expected = moodAnalyze.analyzeMood("SAD");
+		Assert.assertEquals(expected, message);
+	}
+
+	@Test
+	public void testinHappyMood() {
+		String message = "I am in Any Mood";
+		String expected = moodAnalyze.analyzeMood("HAPPY");
+		Assert.assertEquals(expected, message);
+	}
 
 }
